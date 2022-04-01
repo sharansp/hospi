b152d85cbbff6fb1403e2295c688c8d505048c22
SHARAN <sharangowda2425@gmail.com>
2022-04-01 20:57:38 +0530

    adding mui lib

---
+++ src/components/Button/index.js
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+const Button = () => {
+    return (
+        <div className="center">
+           <Box
+            sx={{
+                width: 800,
+                height: 600,
+                backgroundColor: '#E5E5E5',
+
+            }}
+        ></Box>  
+        </div>
+        
+    )
+}
+
+export default BillingContainer
---
+++ src/containers/Billing/index.js
@@ -0,0 +1,21 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import './billing.css'
+
+const BillingContainer = () => {
+    return (
+        <div className="center">
+           <Box
+            sx={{
+                width: 800,
+                height: 600,
+                backgroundColor: '#E5E5E5',
+
+            }}
+        /> 
+        </div>
+        
+    )
+}
+
+export default BillingContainer
